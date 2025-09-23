c569c81a1812cb9a93806493d9fc0db2500c509a
ulkukilic <ukilic1@stu.vistula.edu.pl>
2025-09-23 15:32:45 +0200

    calculateGrade

--- script.js
+++ script.js
@@ -5,9 +5,25 @@ let students = [
   { name: 'Elif', lessons: ['Math', 'Science'], grades: [60, 60] }
 ];
 
-const averages = students.map(s => ({
-  name: s.name,
-  average: s.grades.reduce((a, b) => a + b, 0) / s.grades.length
-}));
+// const averages = students.map(s => ({
+//   name: s.name,
+//   average: s.grades.reduce((a, b) => a + b, 0) / s.grades.length
+// }));
 
-console.log(averages);
+// console.log(averages);
+ 
+
+//! Cozumunuz dogru  ama fonsiyon kullanmaliyiz birden fazla ogrenci oldugu icin
+
+function calculateGrade(student)
+{
+ let total = student.grades.reduce((ort, grade) => ort + grade, 0);  
+ let average = total / student.grades.length;
+ return average;    
+
+}
+calculateGrade(students[0]);
+console.log('Ortalama Not: ' + calculateGrade(students[0])); 
+
+// Note : reduce methodu dizideki elemanları tek bir değere  dusurmek  için kullanılır.
+// Ornegin bir dizide birden cok sayi var ve toplama cikarma yapilicka ise reduce methodu kullanılır.
