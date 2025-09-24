8db63b990546ef86a821a87da56bfa387e019840
ulkukilic <ukilic1@stu.vistula.edu.pl>
2025-09-24 15:18:55 +0200

    event managment system

---
+++ Event Participant Management System/script.js
@@ -0,0 +1,39 @@
+//? 📌 Etkinlik Katılımcı Yönetim Sistemi
+// Katılımcılar: (isim, yaş, biletTuru, checkInMi)
+// Fonksiyonlar: yeni katılımcı ekleme, katılımcı silme, check-in yapma,
+//               bilet türüne göre listeleme, kaç kişinin giriş yaptığını sayma.
+// Kullanılan konular: obje, array methods (push, filter, find, forEach, reduce), if/else.
+
+
+let attendees = [
+{name: 'John', age: 25, ticketType: 'VIP', checkedIn: false},
+{name: 'Jane', age: 30, ticketType: 'Regular', checkedIn: true},
+{name: 'Mike', age: 22, ticketType: 'VIP', checkedIn: false},
+{name: 'Emily', age: 28, ticketType: 'Regular', checkedIn: true}
+];
+
+
+function addAttendee(attendees)  // yeni katılımcı ekleme islemi yapilir (push methodu)
+{
+
+}
+function removeAttendee(attendees)  // promtdan girilen kullanicinin ismine ve tickert turune gore silme islemi yapilir (filter)
+{
+
+}
+
+function checkInAttendee(attendees)  // check-in yapma islemi yapilir (find methodu) false ise true yapar true ise zaten check-in yapildi der
+{
+
+}
+
+function listByTicketType(attendees) // bilet türüne göre listeleme islemi yapilir (filter methodu)
+{
+
+}
+
+function countCheckedIn(attendees)  // giriş yapan katılımcıları sayma islemi yapilir (reduce methodu)
+{
+
+}
+
