ce4647df5c42f28abf6869b5bb9f14bcecdfcc68
erdemgir1985 <alpersekker@gmail.com>
2025-10-01 14:57:44 +0200

    mainpage

---
+++ sporSalonu/script.js
@@ -0,0 +1,6 @@
+//? 📌 Spor Salonu Uyelik Takip Sistemi 
+// Bir üye nesnesi oluştur (name, age, membershipType, isActive).
+// Fonksiyonlar: showInfo (bilgileri göster), activateMembership (üyeliği aktif et), deactivateMembership (üyeliği pasif yap).
+// this methodu kullanilmlai 3 fonsiyon icinde de
+
+const 
